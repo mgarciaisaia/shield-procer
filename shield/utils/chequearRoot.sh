7178976df6f5412461fd26364266ceed57e12055
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-09-11 22:10:47 -0300

    chequearRoot.sh hace el test inline

--- shield/utils/chequearRoot.sh
+++ shield/utils/chequearRoot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 ## TODO: agregar un parametro -s que evite los echo (para usar desde el make)
 
-## FIXME: meter inline el idUsuario
-idUsuario=$(id -u)
-if (( $idUsuario )); then
+if [ "$(id -u)" -ne 0 ]; then
 	echo "No sos root :("
 	exit -1
 else
