e1cecf7c56a7187b72d430d45a044f8ccb6580f0
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-09-11 21:38:36 -0300

    makefile chequea que el usuario sea root

--- shield/utils/chequearRoot.sh
+++ shield/utils/chequearRoot.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+## TODO: agregar un parametro -s que evite los echo (para usar desde el make)
+
+## FIXME: meter inline el idUsuario
 idUsuario=$(id -u)
 if (( $idUsuario )); then
 	echo "No sos root :("
+	exit -1
 else
 	echo "Root para todos!! :D";
 fi
