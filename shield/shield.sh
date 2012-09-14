d84e5356c19b77a95005a8de83593891ec088d70
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-09-14 20:19:09 -0300

    Prompt pedorro (pero prompt al fin)

--- shield/shield.sh
+++ shield/shield.sh
@@ -4,12 +4,10 @@ SCRIPT_SHIELD=$(readlink -f $0)
 DIRECTORIO_SHIELD=$(dirname $SCRIPT_SHIELD)
 
 
-
-
 $DIRECTORIO_SHIELD/utils/verificarPeriodicos.sh &
 while true
 do
-	read -e comando
-	echo "Ingresaste $comando"
+	#TODO: evitar que se cierre con CTRL+C ?
+	read -e -p "> " comando
 	eval $comando
 done
