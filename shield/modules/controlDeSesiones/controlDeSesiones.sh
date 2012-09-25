c4356427acaa049f503e2f1dad383a8b2568f4ca
Federico Alí <fra.ruta66@gmail.com>
2012-09-25 01:52:02 -0300

    Se sube una versión que obtiene las sesiones activas del usuario de la sesión

--- shield/modules/controlDeSesiones/controlDeSesiones.sh
+++ shield/modules/controlDeSesiones/controlDeSesiones.sh
@@ -2,5 +2,21 @@
 
 #Comentario de prueba
 
-echo "hola flandeeeeers, esto es un comienzo";
-exit 1;
+VALOR_RETORNO=1;
+case $1 in
+	informacion) 
+		echo información;;
+	iniciar)
+		echo iniciar
+		USR_SESION=`whoami`;
+		echo $USR_SESION;
+		echo 2
+		LISTA=`who | grep  $USR_SESION`
+		echo 1
+		echo $LISTA
+		;;
+	procesar)
+		echo procesar;;
+esac
+
+exit $VALOR_RETORNO;
