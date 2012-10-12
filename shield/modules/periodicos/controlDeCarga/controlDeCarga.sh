e5be0d2376637df3af2f1ce6569f5e372710b824
Federico Alí <fra.ruta66@gmail.com>
2012-10-12 20:45:12 -0300

    Se sube un cambio de una consulta que obtiene todos los procesos que se est{an ejecutando debajo del shield.

--- shield/modules/periodicos/controlDeCarga/controlDeCarga.sh
+++ shield/modules/periodicos/controlDeCarga/controlDeCarga.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 VALOR_RETORNO=0;
+function procesar(){
+	`ps T -o pcpu,pid,user,nice,tty,args | sort -n -r | grep -v -w shield.sh | grep -v -w /bin/login`
+}
 case $1 in
 	informacion)
 	;;
 	iniciar)
 	;;
 	procesar)
+		procesar;
 	;;
 	detener)
 	;;
