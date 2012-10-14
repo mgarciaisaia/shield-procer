c5b2e1932d84cbcbc16fe322240bf41d945dc72e
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-10-14 08:36:13 -0300

    Builtin mostrar
    Closes #24

--- shield/core/cargarBuiltins.sh
+++ shield/core/cargarBuiltins.sh
@@ -9,3 +9,4 @@ alias ayuda="$DIR_CORE/mostrarAyuda.sh procesar"
 alias apagar="sudo halt -p"
 alias listar_modulos='./listar_modulos.sh'
 alias salir='exit'
+function mostrar { eval echo $`echo $1`; }
