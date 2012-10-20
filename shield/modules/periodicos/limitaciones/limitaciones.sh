bc1d30eff37d2e18aa2d66af1bdecf4a6ae4fdc3
Cristian Rojas <rojas.r.cristian@gmail.com>
2012-10-20 15:47:37 -0300

    Se agrega la funcionalidad para contar los sockets de una sesion

--- shield/modules/periodicos/limitaciones/limitaciones.sh
+++ shield/modules/periodicos/limitaciones/limitaciones.sh
@@ -28,7 +28,6 @@ detener(){
 	unset MAX_CANTIDAD_ARCHIVOS
 }
 
-
 #FALTA EL MAXIMO DE SOCKETS POR SESION
 procesar(){
 
@@ -36,15 +35,15 @@ memoria_total=0
 cpu_total=0
 procesos_totales=0
 archivos_abiertos_totales=0
+sockets_totales=0
 
-#EL FLAG LO UTILIZO PARA NO TENER EN CUENTA LA PRIMER LINEA DEL COMANDO 'ps'
 #procesos_totales=`ps -o pid,pcpu,pmem | awk ' NR >= 1 ' |
 #	awk 'BEGIN { procesos =  0 }
 #	{ procesos++ }
 #	END {print procesos}'`
 
 #FORMA ENCONTRADA EN GOOGLE
-procesos_totales=$(ps -A r | wc -l)
+procesos_totales=$(ps -A r | awk ' NR >= 1 ' | wc -l)
 
 memoria_total=`ps -o pid,pcpu,pmem | awk ' NR >= 1 ' |
 	awk 'BEGIN { memoria = 0 }
@@ -60,18 +59,13 @@ cpu_total=`ps -o pid,pcpu,pmem | awk ' NR >= 1 ' |
 sesion=$(tty)
 archivos_abiertos_totales=`lsof $sesion | wc -l`
 
-#TIRA ERROR
-#=====================================================
-#archivos_abiertos_totales=`ps -o pid,pcpu,pmem |
-#	awk 'BEGIN { archivos_abiertos = 0; flag = 0 }
-#	{ 
-#		if (flag > 0){
-#		archivos_abiertos+=$(find /proc/$1/fd -type l | wc -l)
-#		}
-#		flag = 1;
-#	}
-#'`
-#====================================================
+#CALCULA LA CANTIDAD DE SOCKETS UTILIZADOS POR LA SESION
+for pid in `ps -o pid= `
+	do
+		if [ -d /proc/$pid/fd ] ; then
+			sockets_totales=$((`sudo ls -l /proc/$pid/fd | grep socket: | wc -l` + $sockets_totales))
+		fi
+	done
 
 if (( $MAX_CPU < $cpu_total )) ; then
 	echo "error"
@@ -83,6 +77,9 @@ elif (( $MAX_CANTIDAD_PROCESOS < $procesos_totales )) ; then
 	echo "error"
 #	return error
 elif (( $MAX_CANTIDAD_ARCHIVOS < $archivos_abiertos_totales )) ; then
+	echo "error"
+#	return error
+elif (( $MAX_CANTIDAD_SOCKETS < $sockets_totales )) ; then
 	echo "error"
 #	return error
 else
