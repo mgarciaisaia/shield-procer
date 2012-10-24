9e80de3dff25b3bf393d43d362c8a784fc13c067
Cristian Rojas <rojas.r.cristian@gmail.com>
2012-10-24 14:15:25 -0300

    Se modifica la busqueda de los comandos prohibidos

--- shield/modules/buscarComando.sh
+++ shield/modules/buscarComando.sh
@@ -1,7 +1,16 @@
-BUSQUEDA=`echo ${CONFSEGURIDAD} | grep -o -w "${1}"`
-if [ -z $BUSQUEDA ];
-then
-exit 0
-else
-echo ERROR!!
-fi
+#!/bin/bash 
+
+set -x
+#LEVANTAR POR ARCHIVO DE CONFIGURACION
+CONFSEGURIDAD="ls,mkdir"
+
+#CAMBIA EL DELIMITADOR
+IFS=","
+for comando_prohibido in $CONFSEGURIDAD;
+do
+	RESULTADO=`echo $1 | grep -w $comando_prohibido`
+	if [ ! -z $RESULTADO  ] ; then
+		echo "ERROR"
+	fi
+done 
+
