2573c8dee7fe0d7bec502e56b5104cb45384de42
Cristian Rojas <rojas.r.cristian@gmail.com>
2012-10-24 14:17:08 -0300

    Se corrige el llamado hacia los scripts

--- shield/modules/seguridad.sh
+++ shield/modules/seguridad.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 case $1 in
-	informacion) sh ./darInfo.sh;;
-	iniciar) . ./iniciar.sh;;
+	informacion) ./darInfo.sh;;
+	iniciar) . /iniciar.sh;;
 	detener) unset CONFSEGURIDAD;;
-	procesar) sh ./buscarComando.sh $2;;
+	procesar) ./buscarComando.sh $2;;
 	*) echo "No es una accion que pueda realizar el modulo de seguridad" ;;
 esac
