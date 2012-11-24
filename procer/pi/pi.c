da85cb6d98715b0b44bc1b29864494814bbb2b84
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-11-24 00:18:03 -0300

    PI: loggeo todos los mensajes que vienen del PP
    Closes #41

--- procer/pi/pi.c
+++ procer/pi/pi.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -6,9 +7,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
 #include "commons/network.h"
+#include "commons/log.h"
 
 #define ERROR_PARAMETROS 1
 #define NO_PP_IP 2
@@ -45,59 +46,71 @@ int main(int argc, char* argv[]) {
 	printf("Nos conectamos a %s:%s\n", direccionPP, puertoPP);
 	
 	int conexion = socket(PF_INET, SOCK_STREAM, 0);
-	if(connect(conexion, (struct sockaddr*) socket_address(inet_addr(direccionPP), atoi(puertoPP)), sizeof(struct sockaddr_in))) {
+	struct sockaddr *remote_address = (struct sockaddr*) socket_address(inet_addr(direccionPP), atoi(puertoPP));
+	if(connect(conexion, remote_address, sizeof(struct sockaddr_in))) {
 		printf("Error %d conectandose a %s:%s: %s\n", errno, direccionPP, puertoPP, strerror(errno));
 		exit(errno);
 	}
         
+        free(remote_address);
+        
         int *pid = NULL;
-        int bytesRecibidos = socket_receive(conexion, &pid);
+        int bytesRecibidos = socket_receive(conexion, (void*)&pid);
         if(bytesRecibidos <= 0) {
             perror("Error recibiendo el PID del proceso");
             exit(errno);
         }
         
-        printf("El PID del proceso es %d\n", *pid);
-	
+	char *nombreArchivoLog = NULL;
+        asprintf(&nombreArchivoLog, "PI.[%d].log", *pid);
+        
+        t_log *log = log_create(nombreArchivoLog, "PI", 1, LOG_LEVEL_TRACE);
+        free(nombreArchivoLog);
+        
+        log_info(log, "Ejecutando el proceso con PID %d", *pid);
+        
 	#define TAMANIO_BUFFER 1024
 	char buffer[TAMANIO_BUFFER];
 	int bytesLeidos = 0;
 	int bytesEnviados = 0;
 	
 	while((bytesLeidos = read(script, &buffer, TAMANIO_BUFFER)) > 0) {
+		log_debug("Leidos %d bytes", bytesLeidos);
 		if((bytesEnviados = socket_send(conexion, buffer, bytesLeidos)) > 0) {
-			printf("Envie %d de %d bytes por %d\n", bytesEnviados, bytesLeidos, conexion);
+			log_debug("Envie %d de %d bytes por %d", bytesEnviados, bytesLeidos, conexion);
 		}
 		
-                printf("Lei la linea: %.*s\n", bytesLeidos - 1, buffer + 1);
+                log_debug("Lei la linea: %.*s", bytesLeidos - 1, buffer + 1);
 	}
 	
 	if(close(script)) {
-	       printf("Error %d cerrando el archivo del script: %s\n", errno, strerror(errno));
+	       log_error("Error %d cerrando el archivo del script: %s", errno, strerror(errno));
 	       exit(errno);
 	}
 	
 	char confirmar = 0;
-	char *pedidoReanudar = "1REANUDARPROCESO";
+        #define PEDIDO_REANUDAR "1REANUDARPROCESO"
         char *receiveBuffer = NULL;
 	
 	while((bytesLeidos = socket_receive(conexion, (void**)&receiveBuffer)) > 0) {
 		confirmar = (*receiveBuffer == '1');
-		printf("PROCER dice: %.*s\n", bytesLeidos - 1, receiveBuffer + 1);
+                log_info(log, "%.*s", bytesLeidos - 1, receiveBuffer + 1);
                 free(receiveBuffer);
 		
 		if(confirmar) {
-			printf("Proceso suspendido - presione ENTER para reanudar la ejecucion...\n");
+			printf("Presione ENTER para reanudar la ejecucion...\n");
 			// FIXME: si apreto varios ENTER seguidos, los proximos getchar directamente consmen esas teclas.
 			// Habria que limpiar el buffer o algo asi - no se como.
 			system("stty cbreak -echo");
 			getchar();
 			system("stty cooked echo");
-			socket_send(conexion, pedidoReanudar, strlen(pedidoReanudar) + 1);
-			printf("Proceso reanudado\n");
+			socket_send(conexion, PEDIDO_REANUDAR, strlen(PEDIDO_REANUDAR) + 1);
+			log_info("Proceso reanudado");
 		}
 	}
 
 	close(conexion);
+        log_destroy(log);
+        free(pid);
         return EXIT_SUCCESS;
 }
